60022b4c3dc72c962a567d72285c78f2743de995
Viktor Riabkov <vitya.ryabkov@gmail.com>
2026-01-31 15:01:12 +0800

    feat: configure for custom domain time-tracker.riabkov.com

--- src/router.tsx
+++ src/router.tsx
@@ -4,19 +4,14 @@ import { HomePage } from '@/pages/home-page';
 import { TaskHistoryPage } from '@/pages/task-history-page';
 import { NotFoundPage } from '@/pages/not-found-page';
 
-export const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <RootLayout />,
-      children: [
-        { index: true, element: <HomePage /> },
-        { path: 'history/:taskId', element: <TaskHistoryPage /> },
-        { path: '*', element: <NotFoundPage /> },
-      ],
-    },
-  ],
+export const router = createBrowserRouter([
   {
-    basename: '/work-time-tracker',
-  }
-);
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'history/:taskId', element: <TaskHistoryPage /> },
+      { path: '*', element: <NotFoundPage /> },
+    ],
+  },
+]);
